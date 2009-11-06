d0536c4526dc205490a3abdf69bfa729dd059783
Anders Smestad <anders.smestad@gmail.com>
2009-11-06 13:13:56 +0100

    Updating bat file with jndi options

--- xshell.bat
+++ xshell.bat
@@ -2,4 +2,4 @@
 
 rem TODO: If standalone jar exists, start it, if not, display message telling user to run maven.
 
-java -jar target\XShell-1.0-SNAPSHOT-standalone.jar
+java -jar xshell-core\target\XShell-1.0-SNAPSHOT-standalone.jar -p jndi -name jmx/rmi/RMIAdaptor
